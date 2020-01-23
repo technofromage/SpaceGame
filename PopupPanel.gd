c3861db1369708cf30680f82d0901a08e2d1af6e
technofromage <53595974+technofromage@users.noreply.github.com>
2020-01-23 12:49:55 -0800

    started textproccesing

--- PopupPanel.gd
+++ PopupPanel.gd
@@ -1,5 +1,20 @@
-extends PopupPanel
+extends Popup
 var is_open=false
+var history
+var text
+var functionsG=["forward","backward","left","right"]
+var functionsR=["go","turn"]
+
+func _ready():
+	history=get_child(0)
+	text=get_child(1)
+	history.set_syntax_coloring(true)
+	for word in functionsR:
+		history.add_keyword_color(word,Color.royalblue)
+		text.add_keyword_color(word,Color.royalblue)
+	for word in functionsG:
+		history.add_keyword_color(word,Color.green)
+		text.add_keyword_color(word,Color.royalblue)
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_textOpen"):
@@ -8,7 +23,10 @@ func _process(delta):
 		else:
 			open()
 	if Input.is_action_just_pressed("ui_accept"):#manage input
-		pass
+		var command = text.get_text()
+		text.set_text("")
+		history.set_text(history.get_text()+"\n"+command)
+		history.set_text(history.get_text()+proccess(command))
   
 func open():
 	popup()
@@ -19,3 +37,19 @@ func close():
 	hide()
 	globals.is_movement_locked=false
 	is_open=false
+	
+func proccess(text):
+	var words = text.rsplit(" ",false,2)
+	var pieces
+	for word in words:
+		pieces = word.rsplit(".",false,2)
+		#Note: I dont think godot does a switch method
+		if pieces[0]=="stop":
+			globals.accel=Vector3(0,0,0)
+			globals.rot_accel=Vector3(0,0,0)
+			return "all thrust stoped"
+		if pieces[0]=="go":
+			if pieces[1]=="forward":
+				globals.accel.y=1
+				return "forward thruster set"
+		return "error"
