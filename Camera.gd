a65c6e6a8b9fcffb8687ce39be839ba318f35996
rchpweblo <rchpweblo@gmail.com>
2020-01-28 18:13:22 -0800

    hid mouse
    Hid mouse cursor for better camera feeling

--- Camera.gd
+++ Camera.gd
@@ -4,6 +4,7 @@ extends Camera
 export var sensitivity_vert = 0.001
 
 func _physics_process(delta):
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	#vertical camera rotation
 	if !globals.is_movement_locked:
 		var rotation_vert
